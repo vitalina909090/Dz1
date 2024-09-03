2b86e28c68dcf198567836c430a084434dec9651
vitalina909090 <legezavitalina06@gmail.com>
2024-09-03 22:55:38 +0300

    Добавьте файлы проекта.

---
+++ Dz1.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <vector>
+using namespace std;
+
+class Fraction {
+// за замовчуванням приватна інформація
+    int numerator; //чисельник
+    int denominator; //знаменник
+
+public: 
+    // ввід
+    void input() {
+        cout << "Введіть чисельник: ";
+        cin >> numerator;
+        do {
+            cout << "Введіть знаменник (0 - не можна): ";
+            cin >> denominator;
+            if (denominator == 0) {
+                cout << "Помилка" << endl;
+            }
+        } while (denominator == 0);
+    }
+    // вивід
+    void output() {
+        cout << numerator << "/" << denominator << endl;
+    }
+
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    vector<Fraction> myfractions; 
+    char choice;
+
+    do {
+        Fraction object;
+        object.input();
+        myfractions.push_back(object);
+
+        cout << "Додати ще один дріб? [1]-так [0]-ні: ";
+        cin >> choice;
+    } while (choice == '1');
+
+    cout << "\nВведені дроби:\n";
+    for (int i = 0; i < myfractions.size(); i++) {
+        myfractions[i].output();
+    }
+}
